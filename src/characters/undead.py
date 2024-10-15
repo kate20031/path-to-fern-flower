3bf993e6f8d062b3d13bd24911600e54d1270423
kate20031 <83357098+kate20031@users.noreply.github.com>
2024-10-15 20:16:03 +0300

    Update undead.py

--- src/characters/undead.py
+++ src/characters/undead.py
@@ -1,5 +1,20 @@
 # Питає, чи хочеш дізн. про іст. смерті, якщо ні - кікаєш персонажа,
 # так - розповідь і пропуск ходу.
 class Undead(Silent):
+    def ask_question(self):
+        question = "Do you want to know, how I died?"
+        print(question)
+
+    def tell_story(self):
+        
+    
+    def process_answer(self, answer, life_count):
+        if answer == true:
+            tell_story()
+        else:     
+            life_count -= 1
+        return life_count
+        
     def do_action(self):
-        pass
+        printf("")
+        
