21004f02abe6319c59b2e79a184eee38f3d9ecb8
kate20031 <83357098+kate20031@users.noreply.github.com>
2024-10-24 16:28:29 +0200

    Extracted dialogues and constants for readability. Implemented Peasant class.

--- constants.py
+++ constants.py
@@ -1,8 +1,45 @@
 ASSETS_DIR = 'assets/'
 TEXTS_DIR = f'{ASSETS_DIR}texts/'
 
+#TRAVELER
 TRAVELER_SPEECH_PATH = f'{TEXTS_DIR}traveller_speech.txt'
+
+
+#UNDEAD
 UNDEAD_SPEECH_PATH = f'{TEXTS_DIR}undead_speech.txt'
 
+
+#MAVKA
 MAVKA_SPEECH_PATH = f'{TEXTS_DIR}mavka_speech.txt'
 MAVKA_RIDDLE_PATH = f'{TEXTS_DIR}mavka_riddle.txt'
+
+
+#PEASENT
+# Constants for trade options
+TRADE_YES = 'yes'
+TRADE_NO = 'no'
+PEASANT_INTRO = "Good evening. I'm just a poor peasant, trying to survive in this wilderness."
+PEASANT_TRADE_PROMPT = "Do you have anything to trade?"
+PEASANT_MULTI_ITEM_PROMPT = f"You have several items. Do you want to trade one for a hint? ({TRADE_YES}/{TRADE_NO}): "
+PEASANT_NO_TRADE = "Without a trade, I cannot help you. Perhaps you should head west."
+PEASANT_NO_ITEMS = "You have nothing to trade! Maybe next time."
+PEASANT_TRADE_THANKS = "Thank you for the {}!'"
+PEASANT_REMAINING_SPIRITS = "There are {} spirits you have yet to meet."
+PEASANT_REMAINING_PEOPLE = "There are {} people you have yet to meet."
+PEASANT_NO_ITEMS_FOR_TRADE = "You don't have enough items to trade."
+PEASANT_ACTION = "I am Peasant"
+
+# Minimum items needed for hint trade
+HINT_THRESHOLD = 1
+
+
+# MAN
+MAN_ACTION = "I am Man"
+
+
+# MERCHANT
+MERCHANT_ACTION = "I am Merchant"
+
+
+# BANDIT
+BANDIT_ACTION = "I am Bandit"
--- src/characters/human.py
+++ src/characters/human.py
@@ -1,18 +1,20 @@
 from abc import abstractmethod
-
 from sqlalchemy.sql.functions import random
-
 from ..characters.character import Character
+from constants import *
+
+# Constants for types
+HUMAN_TYPE = 'h'
+SPIRIT_TYPE = 's'
 
 class Human(Character):
-   @abstractmethod
-   def do_action(self):
-      pass
+    @abstractmethod
+    def do_action(self):
+        pass
 
-   def guess_character(self):
-        return input("Is it a human or a spirit? (h/s): ").strip().lower()
+    def guess_character(self):
+        return input(f"Is it a human or a spirit? ({HUMAN_TYPE}/{SPIRIT_TYPE}): ").strip().lower()
 
-# Просто ставить питання.
 class Man(Human):
     def __init__(self, player):
         super().__init__(player)
@@ -21,47 +23,47 @@ class Man(Human):
         pass
 
     def do_action(self):
-        print("I am Man")
+        print(MAN_ACTION)
 
-# Дає підказку за товар, інакше - бреше.
 class Peasant(Human):
     def __init__(self, player):
         super().__init__(player)
         self.hint_given = False
 
     def introduce(self):
-        print("Good evening. I'm just a poor peasant, trying to survive in this wilderness.")
+        print(PEASANT_INTRO)
 
     def do_action(self):
+        print(PEASANT_ACTION)
+        print(PEASANT_TRADE_PROMPT)
 
-        print("I am Peasant. Do you have anything to trade?")
-
-        # Check if player has more than 1 item
-        if len(self.player.items) > 1:
-            answer = input("You have several items. Do you want to trade one for a hint? (yes/no): ").strip().lower()
-            if answer == 'yes':
+        # Check if player has more than 1 item.
+        if len(self.player.items) > HINT_THRESHOLD:
+            answer = input(PEASANT_MULTI_ITEM_PROMPT).strip().lower()
+            if answer == TRADE_YES:
                 self.trade_for_hint()
             else:
-                print("Without a trade, I cannot help you. Perhaps you should head west.")
+                print(PEASANT_NO_TRADE)
         else:
-            print("You have nothing to trade! Maybe next time.")
+            print(PEASANT_NO_ITEMS)
 
     def trade_for_hint(self):
         if self.player.items:
-            traded_item = self.player.items.pop(0)  # Remove one item from player inventory
-            print(f"Thank you for the {traded_item}! Here's your hint: 'The spirits are always truthful.'")
+            traded_item = self.player.items.pop(0)  # Remove one item from player inventory.
+            print(PEASANT_TRADE_THANKS.format(traded_item))
+
+            # Randomly decide whether to give player hint about people or a spirits.
             human_or_spirit = random(0, 1)
 
             if human_or_spirit == 1:
                 remaining_spirits = self.player.remaining_spirits
-                print(f"There are {remaining_spirits} spirits you have yet to meet.")
+                print(PEASANT_REMAINING_SPIRITS.format(remaining_spirits))
             else:
                 remaining_people = self.player.remaining_people
-                print(f"There are {remaining_people} people you have yet to meet.")
+                print(PEASANT_REMAINING_PEOPLE.format(remaining_people))
         else:
-            print("You don't have enough items to trade.")
+            print(PEASANT_NO_ITEMS_FOR_TRADE)
 
-# Продає товар молоко, від духів, від бандита який потім можна виміняти.
 class Merchant(Human):
     def __init__(self, player):
         super().__init__(player)
@@ -70,9 +72,8 @@ class Merchant(Human):
         pass
 
     def do_action(self):
-        print("I am Merchant")
+        print(MERCHANT_ACTION)
 
-# Дає шанс відкупитись, інакше - :(
 class Bandit(Human):
     def __init__(self, player):
         super().__init__(player)
@@ -81,4 +82,4 @@ class Bandit(Human):
         pass
 
     def do_action(self):
-        print("I am Bandit")
+        print(BANDIT_ACTION)
